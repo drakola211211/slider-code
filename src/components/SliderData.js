5a2c7814bec3c731fdc4a62773d8393b8fb4a27a
drakola211211 <sekakoko98765@gmail.com>
2021-01-09 18:19:56 +0200

    support code

---
+++ src/components/SliderData.js
@@ -0,0 +1,24 @@
+import slider1 from '../images/1.jpg'
+import slider2 from '../images/2.jpg'
+import slider3 from '../images/3.jpg'
+import slider4 from '../images/4.jpg'
+import slider5 from '../images/5.jpg'
+
+
+export const SliderData = [
+    {
+        image: slider1
+    },
+    {
+        image: slider2
+    },
+    {
+        image: slider3
+    },
+    {
+        image: slider4
+    },
+    {
+        image: slider5
+    }
+]
